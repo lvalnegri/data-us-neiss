59f4fc36c3f00bb92eaf2a3cd8d15afdd62dfb33
datamaps <l.valnegri@datamaps.co.uk>
2022-07-02 02:50:27 +0100

    added "fad"; deletion of "NA" in age, sex and disp; other small bugs

--- data-raw/11-read_files.R
+++ data-raw/11-read_files.R
@@ -3,7 +3,7 @@ Rfuns::load_pkgs('data.table', 'fst', 'readxl')
 in_path <- file.path(ext_path, 'us', 'NEISS')
 out_path <- file.path(dataus_path, 'neiss')
 
-save_dts <- function(y, fn, fst_path, as_rdb = TRUE, as_rda = TRUE, csv_in_pkg = TRUE, csv2zip = FALSE){
+save_dts <- function(y, fn, fst_path, as_rdb = TRUE, dbn = NULL, as_rda = TRUE, csv_in_pkg = TRUE, csv2zip = FALSE){
     write_fst(y, file.path(fst_path, fn))
     if(as_rdb) dd_dbm_do(dbn, 'w', fn, y)
     if(csv_in_pkg){
@@ -25,21 +25,24 @@ y <- rbindlist(lapply(
             \(x){
                 message('Processing ', x)
                 y <- read_xlsx(file.path(in_path, paste0('neiss', x + 2000, '.xlsx')), 1) |> as.data.table() |> setnames(1, 'id')
-                y[, grep('Other', names(y), value = TRUE) := NULL]
+                y[, c('Stratum', 'PSU', grep('Other', names(y), value = TRUE)) := NULL]
                 if(sum(grepl('Narrative_1', names(y))) > 0) setnames(y, 'Narrative_1', 'Narrative')
                 y
             }
 ))
+y <- y[!(is.na(Age) | is.na(Sex) | is.na(Disposition))]
 
 # separate all vars with multiple columns, plus narratives 
 yb <- rbindlist(list( y[, .(id, body_part = Body_Part)], y[!is.na(Body_Part_2), .(id, body_part = Body_Part_2)] ))
+yb[body_part == 87, body_part := NA]
 yd <- rbindlist(list( y[, .(id, diagnosis = Diagnosis)], y[!is.na(Diagnosis_2), .(id, diagnosis = Diagnosis_2)] ))
-yp <- rbindlist(list( y[, .(id, product = Product_1)], y[!is.na(Product_2), .(id, product = Product_2)], y[!is.na(Product_3), .(id, product = Product_3)] ))
+yd[diagnosis == 70, diagnosis := 71]
+yp <- rbindlist(list( y[, .(id, product = Product_1)], y[Product_2 > 0, .(id, product = Product_2)], y[Product_3 > 0, .(id, product = Product_3)] ))
 yn <- y[, .(id, narrative = Narrative)]
 y[, c('Body_Part', 'Body_Part_2', 'Diagnosis', 'Diagnosis_2', 'Product_1', 'Product_2', 'Product_3', 'Narrative') := NULL]
 
 # cleaning
-setnames(y, c('id', 'date', 'age', 'gender', 'race', 'hisp', 'disposition', 'location', 'fire', 'alcohol', 'drugs', 'stratum', 'psu', 'weight'))
+setnames(y, c('id', 'date', 'age', 'gender', 'race', 'hisp', 'disposition', 'location', 'fire', 'alcohol', 'drugs', 'weight'))
 y[, date := as.Date(date)]
 y[age == 0, age := NA]
 y[, sex := NA_character_][gender %in% 1:2, sex := ifelse(gender == 1, 'M', 'F')][, gender := NULL]
@@ -49,9 +52,14 @@ y[hisp == 1, race := 3]
 y[, hisp := NULL]
 y[disposition == 9, disposition := NA]
 y[location == 0, location := NA]
-y[fire == 0, fire := NA]
-y[fire == 2, fire := 0]
-y[fire == 3, fire := 2]
+
+# create FAD
+y[, fad := fire + alcohol * 10 + drugs * 100]
+y[, c('fire', 'alcohol', 'drugs') := NULL]
+# y[fire == 0, fire := NA]
+# y[fire == 2, fire := 0]
+# y[fire == 3, fire := 2]
+
 setcolorder(y, c('id', 'weight', 'date', 'age', 'sex'))
 
 # create "infants" and recode monthly ages into two yearly class
